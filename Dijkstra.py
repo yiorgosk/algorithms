b2f35099e5f84d1ddcfb0b28014017cae0d4672f
Yioryos Karamitsos <38941519+yiorgosk@users.noreply.github.com>
2020-03-18 12:15:41 +0200

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,5 +1,4 @@
-row = 5
-col = 5
+from operator import attrgetter
 
 
 class Cell:
@@ -17,68 +16,74 @@ class Cell:
         return self.distance < other.distance
 
 
-def isInsideGrid(i, j):
-    return 0 <= i < col and 0 <= j < row
-
-
 class ShortestPath:
-    def __init__(self):
-        self.__matrix = [[0 for i in range(col)]
-                       for j in range(row)]
+    def __init__(self, row, col):
+        self.row = row
+        self.col = col
+        self.matrix = [[0 for i in range(self.col)]
+                       for j in range(self.row)]
 
-        self.dist = [[100000 for i in range(col)]
-                     for j in range(row)]
+        self.dist = [[100000 for i in range(self.col)]
+                     for j in range(self.row)]
 
-        self.shortest_path_tree = [[False for i in range(col)]
-                                   for j in range(row)]
+        self.visited = [[False for i in range(self.col)]
+                        for j in range(self.row)]
 
-        self.grid = [['*' for i in range(col)]
-                     for j in range(row)]
+        self.grid = [['*' for i in range(self.col)]
+                     for j in range(self.row)]
+        self.shortest_path_tree = []
 
     def PrintSolution(self, x, y):
         for row in self.grid:
             print(row)
         print("\nThe destination point is " + str(self.matrix[x][y]) + " and it's weight is " + str(self.dist[x][y]))
+        print(self.shortest_path_tree)
+
+    def isInsideGrid(self, i, j):
+        return 0 <= i < self.col and 0 <= j < self.row
 
     def Dijkstra(self, source, destination):
 
-        dx = [-1, 0, 1, 0]
-        dy = [0, 1, 0, -1]
+        dx = [0, 1, 0, -1]
+        dy = [-1, 0, 1, 0]
 
-        st = set()
-        st.add(Cell(0, 0, 0))
+        priority_list = [Cell(0, 0, 0)]
 
-        for i in range(row):
-            for j in range(col):
+        for i in range(self.row):
+            for j in range(self.col):
                 if self.matrix[i][j] == 0:
-                    self.shortest_path_tree[i][j] = True
+                    self.visited[i][j] = True
                     self.grid[i][j] = '#'
                 else:
-                    self.shortest_path_tree[i][j] = False
+                    self.visited[i][j] = False
 
-        for i in range(row):
-            for j in range(col):
+        for i in range(self.row):
+            for j in range(self.col):
                 if self.matrix[i][j] == source:
-                    self.dist[i][j] = self.matrix[i][j]
+                    self.dist[i][j] = 0
                     self.grid[i][j] = 's'
-                    self.shortest_path_tree[i][j] = True
+                    self.visited[i][j] = True
 
-        while st:
+        while priority_list:
 
-            k = st.pop()
+            cell = min(priority_list, key=attrgetter('distance'))
+            self.shortest_path_tree.append(cell.distance)
+            priority_list.remove(cell)
 
             for i in range(4):
 
-                x = k.x + dx[i]
-                y = k.y + dy[i]
+                x = cell.x + dx[i]
+                y = cell.y + dy[i]
 
-                if not isInsideGrid(x, y):
+                if not self.isInsideGrid(x, y):
                     continue
 
-                if self.dist[x][y] > self.dist[k.x][k.y] + self.matrix[x][y] and self.shortest_path_tree[x][y] == False:
-                    self.dist[x][y] = self.dist[k.x][k.y] + self.matrix[x][y]
-                    st.add(Cell(x, y, self.dist[x][y]))
-                    self.shortest_path_tree[x][y] = True
+                if self.dist[x][y] > self.dist[cell.x][cell.y] + self.matrix[x][y] and self.visited[x][y] == False:
+                    if self.dist[x][y] != 100000:
+                        priority_list.remove(Cell(x, y, self.dist[x][y]))
+                    self.dist[x][y] = self.dist[cell.x][cell.y] + self.matrix[x][y]
+                    priority_list.append(Cell(x, y, self.dist[x][y]))
+                    self.visited[x][y] = True
                     self.grid[x][y] = str(self.dist[x][y])
 
                     if self.matrix[x][y] == destination:
@@ -93,6 +98,6 @@ if __name__ == '__main__':
             [88, 124, 0, 21, 140],
             [99, 32, 11, 41, 20]]
 
-    g = ShortestPath()
+    g = ShortestPath(5, 5)
     g.matrix = grid
     print(g.Dijkstra(31, 20))
