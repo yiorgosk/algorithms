ea967b393e552cea3f00c3c9c8631e077357590e
Yioryos Karamitsos <38941519+yiorgosk@users.noreply.github.com>
2020-11-16 13:43:47 +0200

    Update Lifelong_Planning_Astar.py

--- Lifelong_Planning_Astar.py
+++ Lifelong_Planning_Astar.py
@@ -1,4 +1,5 @@
 from operator import attrgetter  # method for working with specific attributes of an object
+import numpy as np
 
 D = 1  # heuristic function weights
 D2 = 2  # with 2 works better
@@ -19,9 +20,6 @@ class Lifelong_Planning_Astar:  # main class
         self.__dist = [[100000 for i in range(self.__col)]  # matrix for applying 'infinite' weight for each cell that has not been visited yet by Dijkstra function
                        for j in range(self.__row)]
 
-        self.__right_hand_side = [[100000 for i in range(self.__col)]  # matrix for applying 'infinite' weight for each cell that has not been visited yet by LPA* function
-                                  for j in range(self.__row)]
-
         self.__visited = [[False for i in range(self.__col)]  # matrix for the distinction of whether a cell has been visited or not, indicated by True or False
                           for j in range(self.__row)]
 
@@ -34,13 +32,14 @@ class Lifelong_Planning_Astar:  # main class
 
     class __Cell:  # class for storing the cell coordinates, the weight of distance leading to that cell and the f value
 
-        def __init__(self, x, y, distance, rhs, key):
+        def __init__(self, x, y, gscore, rhs, key, parent):
 
             self.x = x
             self.y = y
-            self.distance = distance
+            self.gscore = gscore
             self.rhs = rhs
             self.key = key
+            self.parent = parent
 
     def __Heuristic(self, xn, yn, xg, yg):  # Manhattan heuristic function in order to find the heuristic value that represents the distance from he current node to the destination node
 
@@ -69,16 +68,16 @@ class Lifelong_Planning_Astar:  # main class
                     continue
 
                 if not self.__visited[x][y]:  # if the cell is not contained inside self.__visited matrix then proceed
-                    if self.__dist[x][y] > current_node.distance + self.__matrix[x][y]:  # if the weight (distance) of the cell in check is greater than the value of the source cell plus the cell in check in the main matrix the proceed
-                        self.__right_hand_side[x][y] = current_node.distance + self.__matrix[x][y]  # Applies the g(s') + c(s, s') to rhs value
-                        self.__right_hand_side[x][y] = min(self.__right_hand_side[x][y], self.__dist[x][y])
-                        key = self.Calculatekey(self.__right_hand_side[x][y], self.__dist[x][y], self.__Heuristic(x, y, goal.x, goal.y))  # Calculates the key
+                    if self.__dist[x][y] > current_node.gscore + self.__matrix[x][y]:  # if the weight (distance) of the cell in check is greater than the value of the source cell plus the cell in check in the main matrix the proceed
+                        current_node.rhs = current_node.gscore + self.__matrix[x][y]  # Applies the g(s') + c(s, s') to rhs value
+                        current_node.rhs = min(current_node.rhs, self.__dist[x][y])
+                        key = self.Calculatekey(current_node.rhs, self.__dist[x][y], self.__Heuristic(x, y, goal.x, goal.y))  # Calculates the key
 
-                        node = Lifelong_Planning_Astar.__Cell(x, y, self.__dist[x][y], self.__right_hand_side[x][y], key)  # Creates a new node
+                        node = Lifelong_Planning_Astar.__Cell(x, y, self.__dist[x][y], current_node.rhs, key, current_node)  # Creates a new node
 
                         if node in self.__open_list:
                             self.__open_list.remove(node)
-                        if node.rhs != node.distance:
+                        if node.rhs != node.gscore:
                             self.__open_list.append(node)  # append this cell/node in the priority list
 
     def __PrintSolution(self, x, y, fscore):  # prints the final grid and shortest path
@@ -86,7 +85,14 @@ class Lifelong_Planning_Astar:  # main class
         for row in self.__grid:
             print(row)
         print("\nThe destination point is " + str(self.__matrix[x][y]) + " and it's weight is " + str(fscore))
-        print(self.__closed_list)
+
+        i = 0
+        for node in self.__closed_list:
+            try:
+                print(i, node.gscore, node.rhs, node.key, node.parent.key)
+                i += 1
+            except AttributeError:
+                pass
 
     def __isInsideGrid(self, i, j):  # indicating whether a cell is inside the boundaries of the grid or not
         return 0 <= i < self.__col and 0 <= j < self.__row
@@ -97,55 +103,54 @@ class Lifelong_Planning_Astar:  # main class
             for j in range(self.__col):
                 if self.__matrix[i][j] == 0:
                     self.__visited[i][j] = True
-                    self.__grid[i][j] = '#'
+                    self.__grid[i][j] = '#####'
 
         for i in range(self.__row):  # initializing the source cell
             for j in range(self.__col):
                 if i == xs and j == ys:
                     self.__matrix[i][j] = 0
                     self.__dist[i][j] = 100000
-                    self.__right_hand_side[i][j] = 0
                     self.__grid[i][j] = 's'
 
-        start_node = self.__Cell(xs, ys, self.__dist[xs][ys], self.__right_hand_side[xs][ys],
-                                                    self.Calculatekey(self.__right_hand_side[xs][ys], self.__dist[xs][ys], self.__Heuristic(xs, ys, xd, yd)))
-        goal_node = self.__Cell(xd, yd, self.__dist[xd][yd], self.__right_hand_side[xd][yd],
-                                                   self.Calculatekey(self.__right_hand_side[xd][yd], self.__dist[xd][yd], self.__Heuristic(xd, yd, xd, yd)))
+        start_node = self.__Cell(xs, ys, self.__dist[xs][ys], 0, self.Calculatekey(0, self.__dist[xs][ys], self.__Heuristic(xs, ys, xs, ys)), None)
+        goal_node = self.__Cell(xd, yd, self.__dist[xd][yd], 100000, self.Calculatekey(100000, self.__dist[xd][yd], self.__Heuristic(xd, yd, xs, ys)), None)
         self.__open_list = [start_node]  # priority list that stores the cell that have been visited by the algorithm
         current_node = min(self.__open_list, key=attrgetter('key'))
 
-        while current_node.key < self.Calculatekey(goal_node.rhs, goal_node.distance, self.__Heuristic(goal_node.x, goal_node.y, goal_node.x, goal_node.y)) or goal_node.rhs != goal_node.distance:  # main loop
+        while current_node.key < self.Calculatekey(goal_node.rhs, goal_node.gscore, self.__Heuristic(goal_node.x, goal_node.y, goal_node.x, goal_node.y)) or goal_node.rhs != goal_node.gscore:  # main loop
 
             current_node = min(self.__open_list, key=attrgetter('key'))
-            self.__closed_list.append(current_node.key)  # is set to True in the visited matrix
+            self.__closed_list.append(current_node)  # is set to True in the visited matrix
+
             if current_node in self.__open_list:
                 self.__open_list.remove(current_node)
-            self.__grid[current_node.x][current_node.y] = str(
-                current_node.key)  # update the weight of the target cell/node in the grid
+            self.__grid[current_node.x][current_node.y] = str(current_node.key)  # update the weight of the target cell/node in the grid
 
-            if current_node.x == xd and current_node.y == yd:  # if the coordinates of the target cell are the same as those of the destination cell
-                self.__grid[current_node.x][
-                    current_node.y] = 'd'  # update the value of the target cell with the letter 'd' indicating destination
-                return self.__PrintSolution(current_node.x, current_node.y,
-                                            current_node.key)  # return the _PrintSolution method in order to print the final grid and the shortest path leading to the destination cell
-
-            if current_node.distance > current_node.rhs:
-                current_node.distance = current_node.rhs
+            if self.__dist[current_node.x][current_node.y] > current_node.rhs:
+                current_node.gscore = current_node.rhs
+                self.__dist[current_node.x][current_node.y] = current_node.rhs
                 self.Update(current_node, goal_node)
             else:
-                current_node.distance = 100000
+                current_node.gscore = 100000
+                self.__dist[current_node.x][current_node.y] = 100000
                 self.Update(current_node, goal_node)
+
+            if current_node.x == xd and current_node.y == yd:  # if the coordinates of the target cell are the same as those of the destination cell
+                self.__grid[current_node.x][current_node.y] = 'dest'  # update the value of the target cell with the letter 'd' indicating destination
+                current_node.gscore = current_node.rhs
+                return self.__PrintSolution(current_node.x, current_node.y, current_node.key)  # return the _PrintSolution method in order to print the final grid and the shortest path leading to the destination cell
+            
 
 
 if __name__ == '__main__':
-    grid = [[1, 1, 1, 1, 1, 0, 1, 1, 1, 1],  # main grid
-            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
-            [1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+    grid = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],  # main grid
+            [1, 1, 0, 0, 1, 1, 1, 1, 1, 1],
+            [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
